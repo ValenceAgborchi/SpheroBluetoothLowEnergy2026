32df641235ffd8ea497bb5ecf12f2c5b79830822
ValenceAgborchi <vagborchi27@gmail.com>
2026-06-05 16:27:25 -0400

    Grabbed HTML button by id for every button using getElementById, and added event listener that sends an http request to flask and returns data in javascript object format, and prints a message for each button in textarea terminal

--- index.js
+++ index.js
@@ -1,7 +1,69 @@
 document.getElementById('connecttosphero').addEventListener('click', async () => {
-    const result = await fetch('http://localhost:5000/connect', { method :'POST' })
+    const result = await fetch('http://localhost:5000/connect', {method:'POST'})
     const data = await result.json()
-    document.getElementById('console').value += '/n Connected!'
+    document.getElementById('console').value += '\n Connected!'
 
+})
+
+document.getElementById('disconnectsphero').addEventListener('click', async () => {
+    const result = await fetch('http://localhost:5000/disconnect', {method: 'POST'})
+    const data = await result.json()
+    document.getElementById('console').value += '\n Disconnected'
+
+})
+
+
+document.getElementById('red').addEventListener('click', async () => {
+    const result = await fetch('http://localhost:5000/colour', {
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({r: 255, g: 0, b: 0})
+
+    })
+    document.getElementById('console').value += '\n Colour changed to Red'
+})
+
+document.getElementById('green').addEventListener('click', async () => {
+    const result = await fetch('http://localhost:5000/colour', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({r: 0, g: 255, b: 0})
+    })
+
+    document.getElementById('console').value += '\n Colour changed to Green'
+})
+
+
+document.getElementById('blue').addEventListener('click', async () => {
+    const result = await fetch('http://localhost:5000/colour', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({r: 0, g: 0, b: 255})
+
+
+    })
+ 
+    document.getElementById('console').value += '\n Colour changed to Blue'
+})
+
+
+
+document.getElementById('roll').addEventListener('click', async () => {
+    const result = await fetch('http://localhost:5000/roll', {method: 'POST'})
+    const data = await result.json()
+    document.getElementById('console').value += '\n Rolling......'
+
+})
+
+document.getElementById('spin').addEventListener('click', async () => {
+    const result = await fetch('http://localhost:5000/spin', {method: 'POST'})
+    const data = await result.json()
+    document.getElementById('console').value += '\n Spinning...'
+})
+
+document.getElementById('stop').addEventListener('click', async () => {
+    const result = await fetch('http://localhost:5000/stop', {method: 'POST'})
+    const data = await result.json()
+    document.getElementById('console').value += '\n Stopped.'
 })
 
